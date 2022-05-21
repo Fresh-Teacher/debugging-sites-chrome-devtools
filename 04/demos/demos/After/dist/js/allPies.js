f3b20cf4ac7578910e6632441996d37fda66dd7c
Jesse Paul <freshteacheruganda256@gmail.com>
2022-05-21 15:11:30 +0300

    initial commit

---
+++ 04/demos/demos/After/dist/js/allPies.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+  loadAllPies();
+  // loadNutritionInfo();
+}
+
+function loadAllPies() {
+  let pieTable = document.getElementById('pieTableBody');
+
+  fetch('/api/pies')
+  .then(response => response.json())
+  .then(data => {
+
+    // console.groupCollapsed('pie data');
+    // console.dir(data);
+    // console.table(data);
+    // console.groupEnd();
+
+    // console.assert(data.length === 11, {pieCount: data.length, reason: 'Wrong number of pies'});
+
+    let string_data = JSON.stringify(data);
+
+    let pieMarkup = '';
+
+    data.forEach(element => {
+      pieMarkup += `
+        <tr>
+          <td><img src=${element.imageURL} width="100"></td>
+          <td>${element.name}</td>
+          <td>${element.description}</td>
+          <td>${element.price}</td>
+          <td>View Details</td>
+        </tr>
+      `;
+    });
+
+    pieTable.innerHTML = pieMarkup;
+
+    // console.trace();
+  });
+}
+
+function loadNutritionInfo() {
+  
+  console.info('Attempting to retrieve the nutritional information from the server.');
+
+  fetch('/api/nutrition')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => console.error('There was a problem retrieving the nutritional information.'));
+
+  console.warn('Eating too much pie is not good for!');
+}
